0acef29c15ea87e9f26a4284bddbfe1b3d98d3c7
Shlomi Fish <shlomif@shlomifish.org>
2020-02-25 13:59:42 +0200

    add docs

--- App-gimpgitbuild/lib/App/gimpgitbuild.pm
+++ App-gimpgitbuild/lib/App/gimpgitbuild.pm
@@ -18,6 +18,20 @@ App-gimpgitbuild - build GIMP from git
 
     gimpgitbuild build
 
+=head1 DESCRIPTION
+
+gimpgitbuild is a command line utility to automatically build
+L<GIMP|https://www.gimp.org/> (= the "GNU Image Manipulation Program")
+and some of its dependencies from its version control git repositories:
+L<https://developer.gimp.org/git.html> .
+
+Use it only if your paths and environment does not contain too many
+nasty characters because we interpolate strings into the shell a lot.
+
+So far, it is quite opinionated, but hopefully we'll allow for better
+customization using L<https://en.wikipedia.org/wiki/Environment_variable>
+in the future.
+
 =head1 SEE ALSO
 
 =over 4
